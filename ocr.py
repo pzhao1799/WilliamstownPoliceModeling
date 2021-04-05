8c5e02fbf2c5ddfc50ceaea14fec8a9c26014035
Peter Zhao <pzhao1799@gmail.com>
2021-04-05 16:05:57 -0400

    cleaned up the code and organized with cmd line args

--- ocr.py
+++ ocr.py
@@ -1,51 +1,96 @@
-import pytesseract as pt
+import argparse
+import os
+import sys
 import pdf2image as pdf
+import pytesseract as pt
 from PIL import Image
-from deskew_utils import determine_skew
 
+# Dependencies
 # pip3 install pytesseract, Pillow, pdf2image, scikit-image, numpy
 # sudo apt-get install tesseract-ocr, poppler-utils
 
-prepend = "./low_res"
-page_file = prepend + "/test.pdf"
-pages = pdf.convert_from_path(page_file, dpi = 400)
+# Directory Structure
 
-print("Finished page generation")
-
-i = 1
-for page in pages:
-    image_name = prepend + "/images/page_" + str(i) + ".png"  
-    page.save(image_name, "PNG")
-    i+=1
-    print("Finished a page...")
-
-outfile = prepend + "/out_file.txt"  
-fp = open(outfile, "w")
-
-print("Finished pdf to image...")
-
-for i in range(1, i):
-
-    filename = prepend + "/images/page_" + str(i) + ".png"
-    image_file = Image.open(filename)
-    image_file= image_file.convert('L') # convert image to monochrome - this works
-    image_file= image_file.convert('1') # convert image to black and white 
-    # image_file = image_file.rotate(17)
-    # angle = determine_skew(np.array(image_file)) 
-    # print(angle)
-    # image_file= image_file.rotate(angle)
-    #osd = pt.image_to_osd(image_file)
-    # print(osd)
-    # angle = re.search('(?<=Rotate: )\d+', osd).group(0)
-    # print("angle: ", angle)
-
+def pdf_to_images(pdf_path, png_path, offset):
+    """
+    Takes in a path pdf_path to a PDF file, and saves them in another directory of 
+    png images numbered with offset.
+    """
+    pages = pdf.convert_from_path(pdf_path, dpi = 400)
+    print("Finished pdf to image page generation")
+    for page in pages:
+        image_name = png_path + "/page_" + str(offset) + ".png"  
+        offset+=1
+        page.save(image_name, "PNG")
+        print("Finished page to image conversion...")
+    
+    return offset
+    
+def ocr(png_path, fp):
+    """
+    Takes in a path png_path to a directory of images files, and appends to the file specified by
+    fp
+    """
+    image_file = Image.open(png_path)
+    image_file = image_file.convert('L') # convert image to monochrome
+    image_file = image_file.convert('1') # convert image to black and white 
     text = str(((pt.image_to_string(image_file, config='--psm 6'))))
-    text = text.replace('-\n', '') # remove hyphens at end of lines
     fp.write(text)
     print("OCR'd a page...")
 
-print("Finished image to text...")
+def directory_apply_pdf2images(pdf_path, png_path):
+    """
+    Iterates through each file in the directory specified by dir_path, and applies the given function
+    to each file, if each file is file_type.
+    """
+    offset = 1
+    for entry in os.scandir(pdf_path):
+        if (entry.path.endswith(".pdf") and entry.is_file()):
+            print("!: " + entry.path)
+            offset = pdf_to_images(entry.path, png_path, offset)
+    print("PDF to Image conversion complete")
+            
+def directory_apply_ocr(png_path, output):
+    """
+    Iterates through each file in the directory specified by dir_path, and applies the given function
+    to each file, if each file is file_type. Appends to output.
+    """
+    fp = open(output, "w")
+    for entry in os.scandir(png_path):
+        if (entry.path.endswith(".png") and entry.is_file()):
+            ocr(entry.path, fp)
+    fp.close()
+    print("OCR complete.") 
 
-fp.close()
 
-print("OCR complete.") 
+if __name__ == "__main__":
+    # Create the parser
+    parser = argparse.ArgumentParser(description='List the content of a folder')
+
+    # Add the arguments
+    parser.add_argument('pdfpath',
+                        metavar='pdfpath',
+                        type=str,
+                        help='the path to pdf directory')
+    parser.add_argument('pngpath',
+                        metavar='pngpath',
+                        type=str,
+                        help='the path to png directory')
+    parser.add_argument('outfile',
+                        metavar='outfile',
+                        type=str,
+                        help='the output file')
+                        
+
+    args = parser.parse_args()
+    print(args)
+
+    pngs = os.listdir(args.pngpath)
+
+    # Need to convert pdfs to pngs
+    if len(pngs) == 0:
+        directory_apply_pdf2images(args.pdfpath, args.pngpath)
+        pngs = os.listdir(args.pngpath)
+
+    # pngs to text
+    directory_apply_ocr(args.pngpath, args.outfile)
