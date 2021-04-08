76ff353077ff689d59cd09735bf36397687f5208
Peter Zhao <pzhao1799@gmail.com>
2021-04-08 12:40:30 -0400

    added cleaned out

--- parse_log.py
+++ parse_log.py
@@ -1,13 +1,15 @@
 import re
-filename = "./test/out.txt"
+filename = "./high_res/out_file.txt"
 
 file = open(filename, "r")
+out = open("cleaned_out.txt", "w")
 
 def clean(text):
     # text = text.replace("", "")
     # text = text.replace("|", "")
     text = text.replace(";", ":")
     text = text.replace("~", "-")
+    text = text.replace("--", "-")
     try:
         first_i = text.find(next(filter(str.isalnum, text)))
     except StopIteration:
@@ -19,14 +21,17 @@ list_of_entries = []
 temp = ""
 for line in file:
     line = clean(line)
-    if line.startswith("20-"):
+    # templine = line.replace(" ", "")
+    if re.match("[0-9]+[-][0-9]+\s", line):
+        print(line)
         list_of_entries.append(temp)
         temp = ""
     temp += line
 
 for e in list_of_entries:
-    print("################################################")
-    print(e)
+    out.write(e)
+    out.write("\n")
 
 
 file.close()
+out.close()
