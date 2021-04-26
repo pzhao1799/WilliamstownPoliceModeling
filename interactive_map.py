87c99a957fc6e3099ec135674d12db3601b371e5
Porter Johnson <porterjohnson@Porters-MacBook-Pro.local>
2021-04-26 02:50:16 -0400

    added map function

---
+++ interactive_map.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+import folium
+from folium.plugins import MarkerCluster
+from geopy.geocoders import Nominatim
+
+def geolocate(v,geo):
+    if v =="N/A":
+        return
+    return geo.geocode(v)
+
+def make_map(df):
+    geolocator = Nominatim(user_agent="sample app")
+    df['loc'] = df['location'].apply(geolocate, args = (geolocator,))
+    df["point"]= df['loc'].apply(lambda loc: tuple(loc.point) if loc else None)
+    df[['lat', 'lon', 'altitude']] = pd.DataFrame(df['point'].to_list(), index=df.index)
+    print(df)
+
+    # center to the mean of all points
+    m = folium.Map(location=df[["lat", "lon"]].mean().to_list(), zoom_start=2)
+
+    # if the points are too close to each other, cluster them, create a cluster overlay with MarkerCluster
+    marker_cluster = MarkerCluster().add_to(m)
+
+    # draw the markers and assign popup and hover texts
+    # add the markers the the cluster layers so that they are automatically clustered
+    for i,r in df.iterrows():
+        if r["location"] != "N/A":
+            location = (r["lat"], r["lon"])
+            folium.Marker(location=location,
+                        popup = r['log'],
+                        tooltip=r['log'])\
+            .add_to(marker_cluster)
+
+    # display the map
+    m
+
+
+    # dave to a file
+    m.save("interactive_map.html")
+
+d = {'log': ["20-1", "20-12", "20-13"], 'location': ["MAIN ST, Massachusetts, 01267", "HARWOOD ST, Massachusetts, 01267", "N/A"]}
+df = pd.DataFrame(data=d)
+make_map(df)
